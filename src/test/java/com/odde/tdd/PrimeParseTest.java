01f6ffe5db3639705587f4845baef0de88fc7d23
Lucas Tong <lucas.tong@refinitiv.com>
2022-06-30 15:30:21 +0800

    3rd

--- src/test/java/com/odde/tdd/PrimeParseTest.java
+++ src/test/java/com/odde/tdd/PrimeParseTest.java
@@ -20,15 +20,18 @@ public class PrimeParseTest {
     //    assertArrayEquals(new Integer[]{13}, primeParseUnderTest.primeParse(13));
   }
 
+  @Test
   public void testPrimeParse_illegal() {
     assertArrayEquals(new Integer[]{}, primeParseUnderTest.primeParse(1));
     assertArrayEquals(new Integer[]{}, primeParseUnderTest.primeParse(-1));
 
   }
 
+  @Test
   public void testPrimeParse_normal() {
     //    assertArrayEquals(new Integer[]{2, 3}, primeParseUnderTest.primeParse(6));
 //    assertArrayEquals(new Integer[]{2, 2, 2}, primeParseUnderTest.primeParse(8));
     assertArrayEquals(new Integer[]{2, 3, 5}, primeParseUnderTest.primeParse(30));
+    assertArrayEquals(new Integer[]{2, 2, 5, 5}, primeParseUnderTest.primeParse(100));
   }
 }
